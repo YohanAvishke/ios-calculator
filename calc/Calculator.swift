b65d2e296275c5e6f45ff7cb5d75dbf19c453b4e
YohanAvishke <yaediriweera@gmail.com>
2025-03-06 00:45:05 +1100

    Setup the rest of the simple functions without implementation

--- calc/Calculator.swift
+++ calc/Calculator.swift
@@ -3,30 +3,55 @@ import Foundation
 class Calculator {
     var currentResult:String?
     
-    func add(firstNum: Int, secondNum: Int) -> String {
-        return String(firstNum + secondNum)
+    func add(firstNum: Int, secondNum: Int) -> Int {
+        return firstNum + secondNum
     }
-    func subtract(firstNum: Int, secondNum: Int) -> String {
-        return String(firstNum - secondNum)
+    func subtract(firstNum: Int, secondNum: Int) -> Int {
+        return firstNum - secondNum
+    }
+    
+    func divide(firstNum: Int, secondNum: Int) -> Int {
+        // secondNum cannot be 0
+        return firstNum / secondNum
+    }
+    
+    func multiply(firstNum: Int, secondNum: Int) -> Int {
+        return firstNum * secondNum
+    }
+    
+    func modulus(firstNum: Int, secondNum: Int) -> Int {
+        return firstNum % secondNum
     }
     
     func calculate(args: [String]) -> String {
+        var args = ["1","+","1"]
         if args.count % 2 == 0 {
             return "Invalid Calucation Expression!"
         }
         
         var result:String = "Invalid Character provided!"
-        let firstNum = Int(args[0])!
-        let secondNum = Int(args[2])!
         
-        switch  args[1] {
-        case "+":
-            result = add(firstNum: firstNum, secondNum: secondNum)
-        case "-":
-            result = subtract(firstNum: firstNum, secondNum: secondNum)
-        default:
-            break
+        for i in stride(from: 1, to: args.count-1, by: 2) {
+            let curentOpChar = args[i]
+            let nextOpChar = args[i+2]
+            
         }
+        
+        for i in stride(from: 0, to: args.count, by: 3) {
+            let firstNum = Int(args[i])!
+            let operatorChar = args[i+1]
+            let secondNum = Int(args[i+2])!
+            
+            switch  operatorChar {
+            case "+":
+                result = add(firstNum: firstNum, secondNum: secondNum)
+            case "-":
+                result = subtract(firstNum: firstNum, secondNum: secondNum)
+            default:
+                break
+            }
+        }
+        
         return result
     }
 }
