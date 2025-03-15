c6be28a3c0bf527c1888100d9a6b4eba32f9ffef
YohanAvishke <yaediriweera@gmail.com>
2025-03-16 00:58:53 +1100

    Add final comments

--- calc/main.swift
+++ calc/main.swift
@@ -2,8 +2,8 @@ import Foundation
 
 var args = ProcessInfo.processInfo.arguments
 
-args.removeFirst()
+args.removeFirst() // First input is not part of the calculation
 
 let calculator = Calculator()
-let result = calculator.calculate(args)
+let result = calculator.calculate(args) // calculate
 print(result)
